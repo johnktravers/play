1b81b622266e6f8c88143d61a50187957581a54f
John Travers <johnk.travers@gmail.com>
2020-02-03 11:57:24 -0700

    Set app port to environment process port, listen on said port
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>

--- app.js
+++ app.js
@@ -18,4 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Play';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+});
+
 module.exports = app;
