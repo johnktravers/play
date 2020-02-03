aa4e49e96dedce43673de5e896b8cabf2055a6bc
Brian Bower <brian@brianbower.com>
2020-02-03 15:41:25 -0700

    Add favorites route and router
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 
 var indexRouter = require('./routes/index');
+var favoritesRouter = require('./routes/api/v1/favorites');
 
 var app = express();
 
@@ -17,12 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v1/favorites', favoritesRouter);
 
-app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Play';
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}`)
-});
 
 module.exports = app;
