67c22ccd2dfed1247690240270a4945f109e736c
Brian Bower <brian@brianbower.com>
2020-02-03 15:46:48 -0700

    Add migration to create a favorites table with columns for title, artistName, genre, and rating
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

---
+++ db/migrations/20200203144754_favorites.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('favorites', function(table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('artistName');
+      table.string('genre').defaultTo('Unknown');
+      table.integer('rating');
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('favorites')
+  ]);
+};
