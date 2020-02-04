a800421623b1d438aa3094ba1407664a51a8063f
Brian Bower <brian@brianbower.com>
2020-02-04 14:36:39 -0700

    Add require database and test database cleaner to clear out the testing database after running

--- tests/add-favorite.spec.js
+++ tests/add-favorite.spec.js
@@ -2,7 +2,15 @@ var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../app');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 describe('Add favorites endpoint', () => {
+  beforeEach(async () => {
+       await database.raw('truncate table favorites cascade');
+    });
+
   test('It can add a new favorite track', async () => {
     const res = await request(app)
       .post("/api/v1/favorites")
